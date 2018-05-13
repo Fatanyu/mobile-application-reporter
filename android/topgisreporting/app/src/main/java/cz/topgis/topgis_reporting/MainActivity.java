64e4d3284be6fbfd9c02aed337de5d022b79a6f0
Michal Šrejber <srejber.michal@email.cz>
2018-05-13 10:50:03 +0200

    addButtonOnClick fixed. Add commentary and few Toasts. Refactoring.

--- android/topgisreporting/app/src/main/java/cz/topgis/topgis_reporting/MainActivity.java
+++ android/topgisreporting/app/src/main/java/cz/topgis/topgis_reporting/MainActivity.java
@@ -4,8 +4,6 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -17,19 +15,32 @@ import android.widget.Toast;
 import cz.topgis.topgis_reporting.basics.Basics;
 import cz.topgis.topgis_reporting.location.GPSLocationManager;
 
+/**
+ * Main app Activity. It also works as controller for history records.
+ */
 public class MainActivity extends AppCompatActivity
 {
-	static final int PERMISSION_GPS = 100;
+	static final int REQUEST_CODE_PERMISSION_GPS = 100;
 
+	/**
+	 * This method will be called after activity creations
+	 * @param savedInstanceState Stored previous state
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//TODO change toolbar!!
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 	}
 
+	/**
+	 * This method is called after Options button (in menu) is created
+	 * @param menu Activity menu
+	 * @return True on success creation
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
@@ -38,6 +49,11 @@ public class MainActivity extends AppCompatActivity
 		return true;
 	}
 
+	/**
+	 * TODO - find out what exactly this method should do
+	 * @param item
+	 * @return
+	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item)
 	{
@@ -55,6 +71,10 @@ public class MainActivity extends AppCompatActivity
 		return super.onOptionsItemSelected(item);
 	}
 
+	/**
+	 * This method invoke AddNewRecord activity and starts listener
+	 * @param view View caller
+	 */
 	public void addButtonOnClick(View view)
 	{
 		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
@@ -66,47 +86,58 @@ public class MainActivity extends AppCompatActivity
 			//                                          int[] grantResults)
 			// to handle the case where the user grants the permission. See the documentation
 			// for ActivityCompat#requestPermissions for more details.
-			ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MainActivity.PERMISSION_GPS);
+			ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MainActivity.REQUEST_CODE_PERMISSION_GPS);
 		}
-		else
+		else // Permission granted
 		{
-			GPSLocationManager.getInstance(this).registerListener(this);
+			GPSLocationManager.getInstance(this).registerListener();
 			Basics.giveMeToast(this);
+			//TODO - start activity AddNewRecord
 		}
 
 	}
 
+	/**
+	 * What happens after openning Activity
+	 */
 	@Override
 	protected void onStart()
 	{
 		super.onStart();
 	}
 
+	/**
+	 * What happens after closing Activity. It stops GPS tracking
+	 */
 	@Override
 	protected void onStop()
 	{
 		super.onStop();
-		GPSLocationManager.getInstance(this).unregisterListener();
+		GPSLocationManager.getInstance(this).unregisterListener(); //Off GPS tracking
 	}
 
+	/**
+	 * When app request some permission, this method is invoked after user allow/deny that permission.
+	 * @param requestCode Unique identifier for request
+	 * @param permissions Array of requested permissions as Strings
+	 * @param grantResults Array of granted/denied values. Every value in permissions[] have value in grandResults
+	 */
 	@Override
 	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
 	{
 		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+		//Manage request code
 		switch(requestCode)
 		{
-			case MainActivity.PERMISSION_GPS:
+			// Request GPS permission from addButtonOnClick
+			case MainActivity.REQUEST_CODE_PERMISSION_GPS:
 			{
-				if (grantResults.length > 0	&& grantResults[0] == PackageManager.PERMISSION_GRANTED)
-				{
-					GPSLocationManager.getInstance(this).registerListener(this);
-				}
-				else
-				{
-					Toast.makeText(this,"Really need that permission", Toast.LENGTH_SHORT).show();
-				}
+				if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED))
+					this.addButtonOnClick(null);
+				else //Do not have permission
+					Toast.makeText(this,"Really need that permission", Toast.LENGTH_SHORT).show(); //TODO
 			}
 		}
-
 	}
 }
--- android/topgisreporting/app/src/main/java/cz/topgis/topgis_reporting/location/GPSLocationManager.java
+++ android/topgisreporting/app/src/main/java/cz/topgis/topgis_reporting/location/GPSLocationManager.java
@@ -1,7 +1,7 @@
 package cz.topgis.topgis_reporting.location;
 
 import android.Manifest;
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -11,86 +11,84 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.widget.Toast;
 
+/**
+ * Blackboxing work with GPS
+ */
 public class GPSLocationManager implements LocationListener
 {
-	private static GPSLocationManager ourInstance = null;
+	private static final String MESSAGE_PROVIDER_DISABLED="Provider is disabled";
+	private static final String MESSAGE_PROVIDER_ENABLED="Provider is enabled";
+
+
+	/**
+	 * Static attribute of this class (Singleton pattern)
+	 */
+	@SuppressLint("StaticFieldLeak")
+	private static GPSLocationManager ourInstance;
+
+	/**
+	 * Instance of LocationManager
+	 */
 	private LocationManager locationManager;
+
+	/**
+	 * Instance of actual Context
+	 */
 	private Context context;
+
+	/**
+	 * Instance of LocationListener (it is this class)
+	 */
 	private LocationListener locationListener;
+
+	/**
+	 * Actual (last known) GPS coordinates
+	 */
 	private GPSLocation actualLocation;
 
+	/**
+	 * Singleton getter
+	 * @param context Actual context
+	 * @return Instance of this class with context from parameter
+	 */
 	public static GPSLocationManager getInstance(Context context)
 	{
-		if (ourInstance == null)
-		{
-			ourInstance = new GPSLocationManager(context);
-		}
-		else
-		{
-			ourInstance.setContext(context);
-		}
+		if (ourInstance == null) ourInstance = new GPSLocationManager(context);
+		else ourInstance.setContext(context);
 		return ourInstance;
 	}
 
+	/**
+	 * Simple setter. Do nothing if parameter is null
+	 * @param context New context
+	 */
 	private void setContext(Context context)
 	{
 		if (context != null) this.context = context;
 	}
 
+	/**
+	 * Private Constructor which needs context
+	 * @param context Actual context (should be from MainActivity)
+	 */
 	private GPSLocationManager(Context context)
 	{
 		this.setContext(context);
+		//Get system service
 		this.locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+		//Set listener (plus this class must implements LocationListener)
 		this.locationListener = this;
+		//Prepare dummy location
 		this.actualLocation = GPSLocation.getDummyLocation();
 	}
 
-	@Override
-	public void onLocationChanged(Location location)
+	/**
+	 * Start tracking GPS location
+	 */
+	public void registerListener()
 	{
-		this.setActualLocation(location);
-	}
-
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras)
-	{
-
-	}
-
-	private void setActualLocation(Location location)
-	{
-		if (location != null)
-			this.actualLocation = (new GPSLocation(location.getLatitude(), location.getLongitude()));
-		else this.actualLocation = GPSLocation.getDummyLocation();
-	}
-
-	@Override
-	public void onProviderEnabled(String provider)
-	{
-
-	}
-
-	@Override
-	public void onProviderDisabled(String provider)
-	{
-	}
-
-	public void registerListener(Activity activity)
-	{
-
-		/*
-		if (ActivityCompat.checkSelfPermission(this.context,
-				Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-				ActivityCompat.checkSelfPermission(this.context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)*/
 		if (ActivityCompat.checkSelfPermission(this.context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
 		{
-			// TODO: Consider calling
-			//    ActivityCompat#requestPermissions
-			// here to request the missing permissions, and then overriding
-			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-			//                                          int[] grantResults)
-			// to handle the case where the user grants the permission. See the documentation
-			// for ActivityCompat#requestPermissions for more details.
 			Toast.makeText(this.context, "Nemas pravo na informace k gps.", Toast.LENGTH_SHORT).show();
 			return;
 		}
@@ -98,9 +96,70 @@ public class GPSLocationManager implements LocationListener
 		this.locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this.locationListener);
 	}
 
+	/**
+	 * Stop tracking GPS location
+	 */
 	public void unregisterListener()
 	{
 		Toast.makeText(this.context, "Vypinam GPS listener", Toast.LENGTH_SHORT).show();
 		this.locationManager.removeUpdates(locationListener);
 	}
+
+	/**
+	 * Simple setter. If location is null, set dummy value. Old value will be always scratched.
+	 * @param location New location
+	 */
+	private void setActualLocation(Location location)
+	{
+		if (location != null) this.actualLocation = (new GPSLocation(location.getLatitude(), location.getLongitude()));
+		else this.actualLocation = GPSLocation.getDummyLocation();
+	}
+
+	//
+	// LocationListener inferface implementation
+	//
+
+	/**
+	 * When phone has new location, set it as GPSLocation
+	 * @param location New actual location
+	 */
+	@Override
+	public void onLocationChanged(Location location)
+	{
+		this.setActualLocation(location);
+	}
+
+	/**
+	 * TODO - for what is this??
+	 * @param provider
+	 * @param status
+	 * @param extras
+	 */
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras)
+	{
+
+	}
+
+	/**
+	 * This method is called when location provider is enabled
+	 * @param provider Provider
+	 */
+	@Override
+	public void onProviderEnabled(String provider)
+	{
+		Toast.makeText(this.context, MESSAGE_PROVIDER_ENABLED,Toast.LENGTH_SHORT).show();
+	}
+
+	/**
+	 * This method is called when location provider is disabled
+	 * @param provider Provider
+	 */
+	@Override
+	public void onProviderDisabled(String provider)
+	{
+		Toast.makeText(this.context, MESSAGE_PROVIDER_DISABLED,Toast.LENGTH_SHORT).show();
+	}
+
+
 }
