d5d2c3654645a7e4f90016534942e6c199fb9812
Michal Šrejber <srejber.michal@email.cz>
2018-05-13 18:08:39 +0200

    ReportType created. Report class finished part 1. Package database moved to cz.topgis.topgis_reporting package

--- android/topgisreporting/app/src/main/java/cz/topgis/topgis_reporting/MainPrototypeActivity.java
+++ android/topgisreporting/app/src/main/java/cz/topgis/topgis_reporting/MainPrototypeActivity.java
@@ -1,23 +1,18 @@
 package cz.topgis.topgis_reporting;
 
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import database.Movie;
-import database.MoviesAdapter;
+import cz.topgis.topgis_reporting.database.Movie;
+import cz.topgis.topgis_reporting.database.MoviesAdapter;
 
 public class MainPrototypeActivity extends AppCompatActivity
 {
